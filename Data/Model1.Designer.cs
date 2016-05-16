94a86dff2592c44ce046b39675fc6338fbfba140
uni1PBN <uni1lab@hotmail.fr>
2016-05-16 20:53:22 +0200

    160516-2053

--- Data/Model1.Designer.cs
+++ Data/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\uni1lab\OneDrive\Projects\Visual Studio 2013\UNI1 Mana\Mana v1\Data\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\uni1lab\Source\Repos\Mana\Data\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
